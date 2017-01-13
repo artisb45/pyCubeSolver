5fe23a69db85323078784867870dc31319c131bc
Artūras Bončkus <abonckus@alna.lt>
2017-01-13 17:19:12 +0200

    Defined Cube structure and added printing functionality

--- cube.py
+++ cube.py
@@ -1,9 +1,51 @@
-class Cube():
-    self.white_side = [['w','w','w'],['w','w','w'],['w','w','w']]
-    self.yellow_side = [['y','y','y'],['y','y','y'],['y','y','y']]
-    self.green_side = [['g','g','g'],['g','g','g'],['g','g','g']]
-    self.blue_side = [['b','b','b'],['b','b','b'],['b','b','b']]
-    self.red_side = [['r','r','r'],['r','r','r'],['r','r','r']]
-    self.orange_side = [['o','o','o'],['o','o','o'],['o','o','o']]
+from termcolor import colored
 
+class Cube(object):
+    def __init__(self):
+        self.white_side = [['#','#','#'],['#','#','#'],['#','#','#']]
+        self.yellow_side = [[colored('#','yellow'),colored('#','yellow'),colored('#','yellow')],
+                            [colored('#','yellow'),colored('#','yellow'),colored('#','yellow')],
+                            [colored('#','yellow'),colored('#','yellow'),colored('#','yellow')]]
+        self.green_side = [[colored('#','green'),colored('#','green'),colored('#','green')],
+                           [colored('#','green'),colored('#','green'),colored('#','green')],
+                           [colored('#','green'),colored('#','green'),colored('#','green')]]
+        self.blue_side = [[colored('#','blue'),colored('#','blue'),colored('#','blue')],
+                          [colored('#','blue'),colored('#','blue'),colored('#','blue')],
+                          [colored('#','blue'),colored('#','blue'),colored('#','blue')]]
+        self.red_side = [[colored('#','red'),colored('#','red'),colored('#','red')],
+                         [colored('#','red'),colored('#','red'),colored('#','red')],
+                         [colored('#','red'),colored('#','red'),colored('#','red')]]
+        self.orange_side = [[colored('#','magenta'),colored('#','magenta'),colored('#','magenta')],
+                            [colored('#','magenta'),colored('#','magenta'),colored('#','magenta')],
+                            [colored('#','magenta'),colored('#','magenta'),colored('#','magenta')]]
+
+    def __str__(self):
+        return """
+       - - -
+      |"""+self.yellow_side[0][0]+"""|"""+self.yellow_side[0][1]+"""|"""+self.yellow_side[0][2]+"""|
+       - - -
+      |"""+self.yellow_side[1][0]+"""|"""+self.yellow_side[1][1]+"""|"""+self.yellow_side[1][2]+"""|
+       - - -
+      |"""+self.yellow_side[2][0]+"""|"""+self.yellow_side[2][1]+"""|"""+self.yellow_side[2][2]+"""|
+ - - - - - - - - - - - -
+|"""+self.red_side[0][0]+"""|"""+self.red_side[0][1]+"""|"""+self.red_side[0][2]+"""|"""+self.green_side[0][0]+"""|"""+self.green_side[0][1]+"""|"""+self.green_side[0][2]+"""|"""+self.orange_side[0][0]+"""|"""+self.orange_side[0][1]+"""|"""+self.orange_side[0][2]+"""|"""+self.blue_side[0][0]+"""|"""+self.blue_side[0][1]+"""|"""+self.blue_side[0][2]+"""|
+ - - - - - - - - - - - -
+|"""+self.red_side[1][0]+"""|"""+self.red_side[1][1]+"""|"""+self.red_side[1][2]+"""|"""+self.green_side[1][0]+"""|"""+self.green_side[1][1]+"""|"""+self.green_side[1][2]+"""|"""+self.orange_side[1][0]+"""|"""+self.orange_side[1][1]+"""|"""+self.orange_side[1][2]+"""|"""+self.blue_side[1][0]+"""|"""+self.blue_side[1][1]+"""|"""+self.blue_side[1][2]+"""|
+ - - - - - - - - - - - -
+|"""+self.red_side[2][0]+"""|"""+self.red_side[2][1]+"""|"""+self.red_side[2][2]+"""|"""+self.green_side[2][0]+"""|"""+self.green_side[2][1]+"""|"""+self.green_side[2][2]+"""|"""+self.orange_side[2][0]+"""|"""+self.orange_side[2][1]+"""|"""+self.orange_side[2][2]+"""|"""+self.blue_side[2][0]+"""|"""+self.blue_side[2][1]+"""|"""+self.blue_side[2][2]+"""|
+ - - - - - - - - - - - -
+      |"""+self.white_side[0][0]+"""|"""+self.white_side[0][1]+"""|"""+self.white_side[0][2]+"""|
+       - - -
+      |"""+self.white_side[1][0]+"""|"""+self.white_side[1][1]+"""|"""+self.white_side[1][2]+"""|
+       - - -
+      |"""+self.white_side[2][0]+"""|"""+self.white_side[2][1]+"""|"""+self.white_side[2][2]+"""|
+       - - -"""
     
+    def solve(self, formula):
+        commands = formula.split()
+        for c in commands:
+            return
+            
+
+c = Cube()
+print(c);            

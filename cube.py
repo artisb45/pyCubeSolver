62c4d450632a91a84514a7c259dbf4ef38ea8755
Artūras Bončkus <abonckus@alna.lt>
2017-01-13 14:26:40 +0200

    Create cube.py. Started coding Cube class.

---
+++ cube.py
@@ -0,0 +1,9 @@
+class Cube():
+    self.white_side = [['w','w','w'],['w','w','w'],['w','w','w']]
+    self.yellow_side = [['y','y','y'],['y','y','y'],['y','y','y']]
+    self.green_side = [['g','g','g'],['g','g','g'],['g','g','g']]
+    self.blue_side = [['b','b','b'],['b','b','b'],['b','b','b']]
+    self.red_side = [['r','r','r'],['r','r','r'],['r','r','r']]
+    self.orange_side = [['o','o','o'],['o','o','o'],['o','o','o']]
+
+    
